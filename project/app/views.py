7d75477e46d278062c64234d545cf435a8739a1e
anabaena4310 <biomatsuda2523@gmail.com>
2022-01-12 17:23:33 +0900

    make speech recognition

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import request
+from django.views.decorators.csrf import csrf_exempt
 import speech_recognition as sr
 
 
@@ -7,23 +8,21 @@ def index(request):
     return render(request, 'index.html')
 
 
+@csrf_exempt
 def record(request):
-    data = request.POST.get('audio_data')
+    audio_wav = request.body
 
-    print(data)
-    with open('audio.wav', 'wb') as f:
-        f.write(data)
-    # get audio from the microphone
-    # r = sr.Recognizer()
-    # with sr.Microphone() as source:
-    #     print("Speak:")
-    #     audio = r.listen(source)
+    with open('audio/audio.wav', 'wb') as f:
+        f.write(audio_wav)
+    r = sr.Recognizer()
+    with sr.AudioFile('audio/audio.wav') as source:
+        audio_data = r.record(source)
+    try:
+        output = " " + r.recognize_google(audio_data, language='ja-JP')
+        print(output)
+    except sr.UnknownValueError:
+        output = "Could not understand audio"
+    except sr.RequestError as e:
+        output = "Could not request results; {0}".format(e)
 
-    # try:
-    #     output = " " + r.recognize_google(audio, language='ja-JP')
-    # except sr.UnknownValueError:
-    #     output = "Could not understand audio"
-    # except sr.RequestError as e:
-    #     output = "Could not request results; {0}".format(e)
-
-    return render(request, 'index.html', {'data': data})
+    return render(request, 'index.html', {'data': output})
