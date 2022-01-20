2c07c3e6cb082d93ccdb348125a19e1b79a6d7b9
anabaena4310 <biomatsuda2523@gmail.com>
2022-01-20 16:08:28 +0900

    make log delete

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,4 @@
-from .views import RecordView
-from .views import RecordTrialView
+from .views import RecordView, RecordTrialView, LogView
 from django.urls import path
 from.import views
 
@@ -9,6 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('record/', RecordTrialView.as_view(), name='record'),
-    path("signup/", views.signup, name="signup"),
-    path("home/record/", RecordView.as_view(), name="record_home"),
+    path('signup/', views.signup, name="signup"),
+    path('home/record/', RecordView.as_view(), name="record_home"),
+    path('log/', LogView.as_view(), name="log"),
+    path('delete/', LogView.as_view(), name="delete"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,18 +1,16 @@
+import re
 from django.shortcuts import render, redirect
 from django.http import request
 from django.views.decorators.csrf import csrf_exempt
 import speech_recognition as sr
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import InputText
 from django.contrib.auth import login
 from .form import UserCreationForm
-from django.contrib.auth import authenticate, login as auth_login, get_user_model
 from django.contrib import messages
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-
-
-User = get_user_model()
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -44,6 +42,20 @@ def signup(request):
     return render(request, 'app/signup.html', context)
 
 
+class LogView(LoginRequiredMixin, ListView):
+    template_name = "app/log.html"
+
+    def get(self, request):
+        if InputText.objects.filter(user=request.user).count() > 0:
+            model = InputText.objects.filter(user=request.user)
+            return render(request, self.template_name, {'model': model})
+
+    def post(self, request):
+        delete_pk = request.POST.get('delete')
+        InputText.objects.filter(pk=delete_pk).delete()
+        return redirect('app:log')
+
+
 class RecordTrialView(TemplateView):
     template_name = 'index.html'
 
@@ -62,7 +74,7 @@ class RecordTrialView(TemplateView):
         return JsonResponse({'data': output})
 
 
-class RecordView(TemplateView):
+class RecordView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
