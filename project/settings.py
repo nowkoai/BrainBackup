c0be7e3ff54dd316cb6a2669c6d247ab99b51471
nowko <nowko@xxxxx.com>
2022-08-15 13:04:32 +0900

    change database

--- project/settings.py
+++ project/settings.py
@@ -67,10 +67,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
