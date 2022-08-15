1a6b0e83454f154f9e0c2f1b83a2082211096ec7
nowko <nowko@xxxxx.com>
2022-08-15 13:33:22 +0900

    static file

--- project/settings.py
+++ project/settings.py
@@ -113,10 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'app/static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'app:index'
