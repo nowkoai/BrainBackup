9bb0734d2d2f98c123582ea017c4d6af1ac7119d
nowko <nowko@xxxxx.com>
2022-08-25 11:48:34 +0900

    "app:index"

--- app/views.py
+++ app/views.py
@@ -23,7 +23,7 @@ def signup(request):
         if form.is_valid():
             user_instance = form.save()
             login(request, user_instance)
-            return redirect("app/login.html")
+            return redirect("app:index")
     else:
         form = UserCreationForm()
 
